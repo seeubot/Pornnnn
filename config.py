8589d6c4c90130c8e1cb21a1ed586bb86880b1a1
MikeyxBotz <dragonbytexmikey@gmail.com>
2025-07-22 16:37:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from os import environ
 
 id_pattern = re.compile(r'^.\d+$')
 
-DS_API_ID = int(environ.get("DS_API_ID", ""))
-DS_API_HASH = environ.get("DS_API_HASH", "")
+DS_API_ID = int(environ.get("DS_API_ID", "21134445"))
+DS_API_HASH = environ.get("DS_API_HASH", "231c18ea7273824491d6bf05425ab74e")
 DS_BOT_TOKEN = environ.get("DS_BOT_TOKEN", "")
 DS_BOT_USERNAME = environ.get("DS_BOT_USERNAME", "") # bot username without @
 DS_LOG_CHANNEL = int(environ.get("DS_LOG_CHANNEL", ""))
@@ -24,20 +24,20 @@ PREMIUM_LIMIT_VIDESI = 15
 # Bot Admins
 try:
     DS_ADMINS=[]
-    for x in (environ.get("DS_ADMINS", "1562935405").split()):
+    for x in (environ.get("DS_ADMINS", "8156708830").split()):
         DS_ADMINS.append(int(x))
 except ValueError:
       raise Exception("Your Admins list does not contain valid integers.")
     
 # Mongodb Database 
-DS_DB_URI = environ.get("DS_DB_URI", "")
-DS_DB_NAME = environ.get("DS_DB_NAME", "")
+DS_DB_URI = environ.get("DS_DB_URI", "mongodb+srv://dragonbytexmikey:vkFfYQyByjm4zCqS@cluster0.akw3kyd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DS_DB_NAME = environ.get("DS_DB_NAME", "susant-botz")
 
 # Force subscribe channel 
 DS_AUTH_CHANNEL = int(environ.get('DS_AUTH_CHANNEL', '')) # give your force subscribe channel id here else leave it blank
 
 # Verification Variables
-DS_API = environ.get("DS_API", "") # shortlink api
+DS_API = environ.get("DS_API", "Shortxlinks.com") # shortlink api
 DS_URL = environ.get("DS_URL", "") # shortlink domain without https://
 DS_VERIFY_TUTORIAL = environ.get("DS_VERIFY_TUTORIAL", "") # how to open link 
 DS_VERIFICATION = bool(environ.get("DS_VERIFICATION", True)) # set True Or False and make sure spelling is correct and first letter capital.
